6f7a1590a717fe8d647ea7d8ef501ba231b5b110
Syedtaqhi <syedtaqhi7557@gmail.com>
2025-08-12 11:49:43 +0000

    see the error  i

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp, getApps } from 'firebase/app';
-import { getAuth, connectAuthEmulator, inMemoryPersistence, setPersistence } from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   "projectId": "devs-tec-collab",
@@ -14,19 +14,4 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
 
-const auth = getAuth(app);
-
-if (typeof window !== 'undefined') {
-    // We only want to run this configuration logic in the browser.
-    // And because it's async, we'll wrap it in an immediately-invoked function expression.
-    (async () => {
-        try {
-            // Connect to the local Auth emulator.
-            connectAuthEmulator(auth, 'http://127.0.0.1:9099', { disableWarnings: true });
-            // This persistence is useful for the development environment.
-            await setPersistence(auth, inMemoryPersistence);
-        } catch (e) {
-            console.error("Could not connect to auth emulator", e);
-        }
-    })();
-}
+getAuth(app);
