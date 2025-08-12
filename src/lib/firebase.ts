f9d2e87c93c9f5bef3e7d839e0e79478011aa6b4
Syedtaqhi <syedtaqhi7557@gmail.com>
2025-08-12 19:36:53 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD5sRzJm_DTpBQCqMBLtYbtMv9Ge6jdUZE",
-  authDomain: "devs-tec-collab.firebaseapp.com",
-  projectId: "devs-tec-collab",
-  storageBucket: "devs-tec-collab.appspot.com",
-  messagingSenderId: "443824033040",
-  appId: "1:443824033040:web:bc9f3f258f67292d73e995",
+  "projectId": "devs-tec-collab",
+  "appId": "1:443824033040:web:bc9f3f258f67292d73e995",
+  "storageBucket": "devs-tec-collab.appspot.com",
+  "apiKey": "AIzaSyD5sRzJm_DTpBQCqMBLtYbtMv9Ge6jdUZE",
+  "authDomain": "devs-tec-collab.firebaseapp.com",
+  "messagingSenderId": "443824033040",
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
