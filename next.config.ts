4e2bb5cec021f9867e69b6dff67575143e27b605
Syedtaqhi <syedtaqhi7557@gmail.com>
2025-10-24 14:06:59 +0000

    aadd   all ths pictures to my back of the addd

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
   experimental: {
