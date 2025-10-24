4da42e5edbeeb7aececaef40bd155708c29678b6
Syedtaqhi <syedtaqhi7557@gmail.com>
2025-10-24 14:13:55 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "devs-tec-collab",
+  "appId": "1:443824033040:web:bc9f3f258f67292d73e995",
+  "apiKey": "AIzaSyD5sRzJm_DTpBQCqMBLtYbtMv9Ge6jdUZE",
+  "authDomain": "devs-tec-collab.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "443824033040"
+};
